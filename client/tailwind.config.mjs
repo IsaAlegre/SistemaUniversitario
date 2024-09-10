8fbcf358a8b0ef92969cfecd52b383d7eb94e78d
johaortiz <surf234@outlook.es>
2024-09-10 10:06:46 -0300

    fix: request

--- client/tailwind.config.mjs
+++ client/tailwind.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-<<<<<<< HEAD
 			'./client/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
 	],
 			 
-=======
-        './client/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
->>>>>>> 82301023271771ef772f834b6abbcf87d7b2c837
 	theme: {
 		extend: {},
 	},
